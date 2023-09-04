423312291697e74e3ef734f29184f8f10009f556
Toby <newtobycm@gmail.com>
2023-09-03 18:22:09 -0700

    oops i forgot to add the file lmao
    Signed-off-by: Toby <newtobycm@gmail.com>

---
+++ src/components/WelcomeMessageWithAdd.tsx
@@ -0,0 +1,43 @@
+import { Add } from "@mui/icons-material";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import NewWebSookDialog from "./WebSook/NewDialog";
+
+export default function WelcomeMessageWithAdd(props: {
+  _openDialog: boolean;
+
+  openDialog: (open: boolean) => void;
+}) {
+  const { _openDialog, openDialog } = props;
+
+  return (
+    <Box>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item xs={3}>
+          <Box sx={{ p: 2 }}>
+            <Typography variant="h3" align="center">
+              Welcome to Webhook 2 WebSocket
+            </Typography>
+            <Typography variant="h6" marginTop={"2vh"} align="center">
+              You have no WebSooks yet. Create a new one by clicking the button
+              below.
+            </Typography>
+            <Box display={"flex"} justifyContent={"center"} mt={"2vh"}>
+              <Button onClick={() => openDialog(true)} startIcon={<Add />}>
+                <Typography variant="h6" align="center">
+                  Create new WebSook
+                </Typography>
+              </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+      <NewWebSookDialog open={_openDialog} close={() => openDialog(false)} />
+    </Box>
+  );
+}
