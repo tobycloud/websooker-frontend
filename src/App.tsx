02735b71b44b062048f2237441d51ccbb49bd00b
Toby <newtobycm@gmail.com>
2023-09-06 13:19:00 -0700

    refactor: :fire: ko bt noi j

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
 import { ThemeProvider } from "@emotion/react";
 import { Box, CssBaseline, createTheme, useMediaQuery } from "@mui/material";
-import { RecordSubscription } from "pocketbase";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import AppBar from "./components/AppBar";
 import WebSookList from "./components/WebSook/HomePageList";
 import Welcome from "./components/WelcomeMessageWithAdd";
 import pocketbase from "./database";
 import LoginPage from "./pages/Login";
-
-function updateAuthStore(event: RecordSubscription) {
-  if (event.action == "delete") {
-    pocketbase.authStore.clear();
-    pocketbase.collection("users").unsubscribe(pocketbase.authStore.model!.id);
-  } else {
-    pocketbase.authStore.save(pocketbase.authStore.token, event.record);
-  }
-}
 
 export default function App() {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -43,19 +33,31 @@ export default function App() {
 }
 
 function RealApp() {
+  // logged in
+
   const [zeroUrls, setZeroUrls] = useState(
     pocketbase.authStore.model!.urls === 0
   );
 
-  pocketbase
-    .collection("users")
-    .subscribe(pocketbase.authStore.model!.id, updateAuthStore);
+  useEffect(() => {
+    pocketbase
+      .collection("users")
+      .subscribe(pocketbase.authStore.model!.id, (event) => {
+        if (event.action == "delete") {
+          pocketbase.authStore.clear();
+          pocketbase
+            .collection("users")
+            .unsubscribe(pocketbase.authStore.model!.id);
+        } else {
+          pocketbase.authStore.save(pocketbase.authStore.token, event.record);
+        }
+      });
 
-  pocketbase.authStore.onChange((_, user) => {
-    if (!user) return;
-    if (user.urls === 0) setZeroUrls(true);
-    else setZeroUrls(false);
-  });
+    pocketbase.authStore.onChange((_, user) => {
+      if (!user) return;
+      setZeroUrls(user.urls === 0);
+    });
+  }, []);
 
   return (
     <>
