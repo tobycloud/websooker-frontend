252cc7118ba2f928cee8f6ff6bbb8f91d3f17936
Toby <newtobycm@gmail.com>
2023-09-05 21:50:31 -0700

    fix: :fire: add env file now, use .env.local for your own variables

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 
 const POCKETBASE_URL = import.meta.env.VITE_POCKETBASE_URL;
-console.log(POCKETBASE_URL);
+
 const pocketbase = new PocketBase(POCKETBASE_URL);
 
 export default pocketbase;
