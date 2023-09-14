d80feb0774b50a3f38cce17a7edc0c04fe24eb3f
Toby Cm <62174797+tobycm@users.noreply.github.com>
2023-09-14 11:51:14 -0700

    fix: :fire: api on different domain ok

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
 export const POCKETBASE_URL = "https://websooker-pocketbase.tobycm.systems";
 export const BASE_WEBSOOKER_URL = "https://websooker.tobycm.systems";
+export const WEBSOOKER_API = "api-websooker.tobycm.system" // without scheme pls
