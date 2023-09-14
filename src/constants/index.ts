bdf4238d9086c6ee46774c9f1030c34d811814f5
Toby Cm <62174797+tobycm@users.noreply.github.com>
2023-09-14 12:43:25 -0700

    fix: :fire: thiếu s lmao

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,2 @@
 export const POCKETBASE_URL = "https://websooker-pocketbase.tobycm.systems";
-export const WEBSOOKER_API = "api-websooker.tobycm.system"; // without scheme pls
+export const WEBSOOKER_API = "api-websooker.tobycm.systems"; // without scheme pls
